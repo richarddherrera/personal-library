6e075992c9cdeda4fea9a31ad9ce9c2509501d8c
Richard Herrera <yrichardz3003@gmail.com>
2025-07-08 17:15:17 -0300

    Projeto inicializado

---
+++ personal-library/src/test/java/com/richarddev/personal_library/PersonalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richarddev.personal_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
