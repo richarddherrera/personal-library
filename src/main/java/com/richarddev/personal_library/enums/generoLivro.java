9fb586a3232c9b3885331baba01798ed7156d320
Richard Herrera <yrichardz3003@gmail.com>
2025-07-08 17:58:47 -0300

    feat(enums): adicionar enum generoLivro

---
+++ src/main/java/com/richarddev/personal_library/enums/generoLivro.java
@@ -0,0 +1,24 @@
+package com.richarddev.personal_library.enums;
+
+public enum generoLivro {
+    ROMANCE,
+    FICCAO_CIENTIFICA,
+    FANTASIA,
+    SUSPENSE,
+    TERROR,
+    BIOGRAFIA,
+    HISTORIA,
+    DRAMA,
+    AVENTURA,
+    COMEDIA,
+    MISTERIO,
+    POESIA,
+    AUTOAJUDA,
+    RELIGIAO,
+    DIDATICO,
+    INFANTIL,
+    JUVENIL,
+    HQ,
+    ENSAIO,
+    TECNICO
+}
