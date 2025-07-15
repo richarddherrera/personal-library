1f789a192c42172aee94ae5aed52dcedbd05c703
Richard Herrera <yrichardz3003@gmail.com>
2025-07-15 17:19:51 -0300

    feat(controller): adicionar endpoint GET do Livro

--- src/main/java/com/richarddev/personal_library/controller/LivroController.java
+++ src/main/java/com/richarddev/personal_library/controller/LivroController.java
@@ -1,15 +1,16 @@
 package com.richarddev.personal_library.controller;
 
 import com.richarddev.personal_library.dto.DadosLivroCreate;
+import com.richarddev.personal_library.dto.DadosLivroRead;
+import com.richarddev.personal_library.dto.DadosLivroUpdate;
 import com.richarddev.personal_library.model.Livro;
 import com.richarddev.personal_library.repository.LivroRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/livros")
@@ -22,5 +23,13 @@ public class LivroController {
     @Transactional
     public void cadastroLivro (@RequestBody @Valid DadosLivroCreate dados){
         livroRepository.save(new Livro(dados));
+    }
+
+    @GetMapping
+    public List<DadosLivroRead> listar(){
+       return livroRepository.findAll()
+               .stream()
+               .map(DadosLivroRead::new) // converte cada Livro em DadosLivroRead
+               .toList();
     }
 }
